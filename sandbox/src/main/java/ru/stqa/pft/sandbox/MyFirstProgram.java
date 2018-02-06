74b787e29fc92141e05bd1dd9f6a6db71cc5d823
Jane Shysh <janeshysh@gmail.com>
2018-02-06 14:52:56 +0200

    Hello + somebody

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,10 +3,11 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    String somebody = "world";
+    System.out.println("Hello, " + somebody + "!");
 
-    int l = 6;
-    int s = l*l;
+    double l = 6.5;
+    double s = l*l;
     System.out.println("Площадь квадрата со стороной " + l + " = " + (s));
   }
 
