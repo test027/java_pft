c313434355ff2c2dbdf1d1b253fda81c3458026c
Jane Shysh <janeshysh@gmail.com>
2018-02-06 14:35:18 +0200

    Площадь квадрата со стороной

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,6 +4,10 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello, world!");
+
+    int l = 6;
+    int s = l*l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + (s));
   }
 
 }
