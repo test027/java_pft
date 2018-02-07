417f8bcc772376c1779a6b6720f436bd7f7a7ef0
Jane Shysh <janeshysh@gmail.com>
2018-02-07 14:13:32 +0200

    Task 2. ч. 4

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -5,9 +5,14 @@ public class Point {
   public double x;
   public double y;
 
-  public Point(double x, double y){
+  public Point(double x, double y) {
     this.x = x;
     this.y = y;
+  }
+
+
+  public double distance(Point second) {
+    return Math.sqrt(Math.pow((second.x - this.x), 2) + Math.pow((second.y - this.y), 2));
   }
 
 }
