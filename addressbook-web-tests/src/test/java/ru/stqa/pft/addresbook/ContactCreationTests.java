6353d4ee5c5b590406330dc8523cb70908940d88
Jane Shysh <janeshysh@gmail.com>
2018-02-08 16:54:15 +0200

    Task4. Фикстуры для ContactCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/ContactCreationTests.java
@@ -22,10 +22,6 @@ public class ContactCreationTests {
     public void setUp() throws Exception {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void ContactCreationTests() {
         wd.get("http://localhost/addressbook/edit.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -34,8 +30,11 @@ public class ContactCreationTests {
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.id("content")).click();
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+    
+    @Test
+    public void testContactCreation() {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
         wd.findElement(By.name("firstname")).sendKeys("Test");
