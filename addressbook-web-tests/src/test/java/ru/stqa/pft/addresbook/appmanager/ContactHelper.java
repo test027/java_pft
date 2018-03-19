56eb54708ba81b918a91293d29f19f5914e59023
Jane Shysh <janeshysh@gmail.com>
2018-03-19 10:07:19 +0200

    up

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/appmanager/ContactHelper.java
@@ -45,4 +45,8 @@ public class ContactHelper extends HelperBase{
   public void submitContactModification() {
     click(By.name("update"));
   }
+
+  public void closeAlert() {
+    wd.switchTo().alert().accept();
+  }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/ContactDeletionTests.java
@@ -8,7 +8,8 @@ public class ContactDeletionTests extends TestBase{
     public void testContactDeletion() {
       app.getContactHelper().selectContact();
       app.getContactHelper().deleteSelectedContacts();
- //     returnToHomePage();
+ //     app.getContactHelper().closeAlert();
+      //     returnToHomePage();
     }
 
 }
